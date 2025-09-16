46e657cfbd5d4e77769700e3d6cf83e9fe6dacd2
william <>
2025-09-16 16:28:04 +0800

    new

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_aliplayer/flutter_aliplayer.dart';
+import 'package:flutter_aliplayer/flutter_aliplayer_factory.dart';
+
+import 'package:flutter_livepush_plugin/base/live_base.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState()  {
+    super.initState();
+    init();
+  }
+
+  void init() async {
+    AlivcLiveBase.registerSDK();
+
+    FlutterAliplayer fAliplayer = FlutterAliPlayerFactory.createAliPlayer();
+    fAliplayer.setOnPrepared((playerId) {});
+
+    // /// 2.设置监听回调接口
+    // AlivcLiveBase.setListener(AlivcLiveBaseListener(
+    //   onLicenceCheck: (AlivcLiveLicenseCheckResultCode result, String reason) {
+    //     if (result == AlivcLiveLicenseCheckResultCode.success) {
+    //       /// 注册SDK成功
+    //     }
+    //   },
+    // ));
+    //
+    // String sdkVersion = await AlivcLiveBase.getSdkVersion();
+    // print('aaaaaa $sdkVersion');
+    //
+    // /// 启用控制台日志打印
+    // AlivcLiveBase.setConsoleEnable(true);
+    //
+    // /// 设置log级别为Debug调试级别
+    // AlivcLiveBase.setLogLevel(AlivcLivePushLogLevel.debug);
+    //
+    // /// 每个分片最大大小，最终日志总体积是最大分片大小的5倍
+    // const int saveLogMaxPartFileSizeInKB = 100 * 1024 * 1024;
+    //
+    // /// 日志路径
+    // String saveLogDir = "TODO";
+    //
+    // /// 设置日志路径及日志分片大小
+    // AlivcLiveBase.setLogPath(saveLogDir, saveLogMaxPartFileSizeInKB);
+
+    print('7777777');
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
